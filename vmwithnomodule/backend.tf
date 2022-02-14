40312c7f71539a9fd57a79224ce0518d3780e515
anilkrs09 <32677716+anilkrs09@users.noreply.github.com>
2022-02-14 15:53:55 +0530

    Delete backend.tf

--- vmwithnomodule/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket         = "spark-49509"
-        region         = "us-east-1"
-        key            = "networking/terraform.tfstate"
-        dynamodb_table = "spark-tfstatelock-49509"
-        encrypt        = true   
-    }
-}
